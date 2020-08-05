ca3e0388c29a316d8437ece6658640aef4a6b2d3
Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-08-05 11:58:04 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -8,80 +8,80 @@ HTML5 Video, Audio, Youtube, Vimeo, Soundcloud, Bandcamp, Audiomack & Web iframe
 		config: {
 			mode: "iframe",						// iframe (choose source below), video or audio
 
-	// HTML5 Video or Audio mode example
-	//		url: "modules/yframe/landscape.mp4",			// local path/folder/video.mp4, audio.mp3 or embeded media
+		// HTML5 Video or Audio mode example
+		//	url: "modules/yframe/landscape.mp4",			// local path/folder/video.mp4, audio.mp3 or embeded media
 
-	// Web source iframe mode example (plus media choice)
-	//		url: "https://cristea13.ro",				// media: "site"
-			url: "https://www.dailymotion.com/embed/video/x7urdc7",	// media: "media"
+		// Web source iframe mode example (plus media choice)
+		//	url: "https://cristea13.ro",				// media: "website"
+			url: "https://www.dailymotion.com/embed/video/x7urdc7",	// media: "streaming"
 
-	// Youtube source iframe mode example
-	//		url: "https://www.youtube.com/embed/eKFTSSKCzWA",
+		// Youtube source iframe mode example
+		//	url: "https://www.youtube.com/embed/eKFTSSKCzWA",
 
-	// Vimeo source iframe mode example
-	//		url: "https://player.vimeo.com/video/54511177",
+		// Vimeo source iframe mode example
+		//	url: "https://player.vimeo.com/video/54511177",
 
-	// Soundcloud source iframe mode example
-	//		url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/653929878",
+		// Soundcloud source iframe mode example
+		//	url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/653929878",
 
-	// Badcamp source iframe mode example
-	//		url: "https://bandcamp.com/EmbeddedPlayer/album=3096747879",
+		// Badcamp source iframe mode example
+		//	url: "https://bandcamp.com/EmbeddedPlayer/album=3096747879",
 
-	// Audiomack source iframe mode example
-	//		url: "https://audiomack.com/embed/playlist/razvanh/love-of-future",
+		// Audiomack source iframe mode example
+		//	url: "https://audiomack.com/embed/playlist/razvanh/love-of-future",
 
-	// global settings
-			width: "1024px",					// for iframe just px not %
-			height: "576px",					// for iframe just px not %
-			cssClass: "video",					// custom className
-			loop: true,						// loop video
-			autoplay: true,						// autoplay video
-			contols: false,						// if autoplay and loop is false you need controls
-			muted: true,						// muted video
+		// global settings
+				width: "1024px",					// for iframe just px not %
+				height: "576px",					// for iframe just px not %
+				cssClass: "video",					// custom className
+				loop: true,						// loop video
+				autoplay: true,						// autoplay video
+				contols: false,						// if autoplay and loop is false you need controls
+				muted: true,						// muted video
 
-	// HTML5 Video or Audio mode
-			type: "video/mp4",					// video/mp4, video/webm, video/ogg or audio/mpeg, audio/ogg, audio/wav
-			poster: "",						// custom poster image
+		// HTML5 Video or Audio mode
+				type: "video/mp4",					// video/mp4, video/webm, video/ogg or audio/mpeg, audio/ogg, audio/wav
+				poster: "",						// custom poster image
 
-	// iframe mode
+		// iframe mode
 			source: "Web",						// Web (choose media below), Youtube, Vimeo, Soundcloud, Bandcap, Audiomack
 
-	// Web source
-			media: "media",						// "site" if url is website link or "media" if url is multimedia link
-			scrolling: "no",
-			allowfullscreen: "yes",
-			allow: "autoplay; fullscreen; encrypted-media; picture-in-picture",
-			frameborder: "0",					// style width color
-			name: "iframe",
+		// Web source - any website without X-Frame-Options deny and sameorigin activated or media from DTube, Metacafe, Ted, Lbry.tv
+			media: "media",						// "website" if url is website link or "streaming" if url is multimedia link
+				scrolling: "no",
+				allowfullscreen: "yes",
+				allow: "autoplay; fullscreen; encrypted-media; picture-in-picture",
+				frameborder: "0",					// style width color
+				name: "iframe",
 
-	// Youtube source
-			related: 0,
+		// Youtube source
+				related: 0,
 
-	// Vimeo source
-			author: 0,
-			portrait: 0,
-			title: 0,
+		// Vimeo source
+				author: 0,
+				portrait: 0,
+				title: 0,
 
-	// Soundcloud source - make sure that muted is not true
-			color: "%23ff5500",
-			hide_related: true,
-			show_comments: false,
-			show_user: true,
-			show_reposts: false,
-			show_teaser: false,
-			visual: false,
+		// Soundcloud source
+				color: "%23ff5500",
+				hide_related: true,
+				show_comments: false,
+				show_user: true,
+				show_reposts: false,
+				show_teaser: false,
+				visual: false,
 
-	// Bandcamp source - make sure that muted is not true
-			size: "large",						// "large" only if minimal is false
-			background_color: "ffffff",				// "RRGGBB" custom colors
-			link_color: "333333",					// "RRGGBB" custom colors
-			tracklist: true,
-			artwork: "small",					// small or big
-			transparent: true,
-			minimal: false,						// true only if size is false
+		// Bandcamp source
+				size: "large",						// "large" only if minimal is false
+				background_color: "ffffff",				// "RRGGBB" custom colors
+				link_color: "333333",					// "RRGGBB" custom colors
+				tracklist: true,
+				artwork: "small",					// small or big
+				transparent: true,
+				minimal: false,						// true only if size is false
 
-	// Audiomack source - make sure that muted is not true
-			background: 1,						// 1 = art background, 0 = no background
-			color: false,						// "RRGGBB" custom colors or false
+		// Audiomack source
+				background: 1,						// 1 = art background, 0 = no background
+				color: false,						// "RRGGBB" custom colors or false
 		},
 	}
--- yframe.js
+++ yframe.js
@@ -10,59 +10,59 @@ Module.register("yframe", {
 		mode: "",								// iframe (choose source below), video or audio
 		url: "",
 
-// global settings
-		width: "1024px",						// for iframe just px not %
-		height: "576px",						// for iframe just px not %
-		cssClass: "video",						// custom className
-		loop: true,								// loop video
-		autoplay: true,							// autoplay video
-		contols: false,							// if autoplay and loop is false you need controls
-		muted: true,							// muted video
+	// global settings
+			width: "1024px",						// for iframe just px not %
+			height: "576px",						// for iframe just px not %
+			cssClass: "video",						// custom className
+			loop: true,								// loop video
+			autoplay: true,							// autoplay video
+			contols: false,							// if autoplay and loop is false you need controls
+			muted: true,							// muted video
 
-// HTML5 Video or Audio mode
-		type: "video/mp4",						// video/mp4, video/webm, video/ogg or audio/mpeg, audio/ogg, audio/wav
-		poster: "",								// custom poster image
+	// HTML5 Video or Audio mode
+			type: "video/mp4",						// video/mp4, video/webm, video/ogg or audio/mpeg, audio/ogg, audio/wav
+			poster: "",								// custom poster image
 
-// iframe mode
+	// iframe mode
 		source: "",								// Web (choose media below), Youtube, Vimeo, Soundcloud, Bandcap, Audiomack
 
-// Web source
-		media: "",								// "site" if url is website link or "media" if url is multimedia link
-		scrolling: "no",
-		allowfullscreen: "yes",
-		allow: "autoplay; fullscreen; encrypted-media; picture-in-picture",
-		frameborder: "0",						// style width color
-		name: "iframe",
+	// Web source - any website without X-Frame-Options deny and sameorigin activated or media from DTube, Metacafe, Ted, Lbry.tv
+		media: "",								// "website" if url is website link or "streaming" if url is multimedia link
+			scrolling: "no",
+			allowfullscreen: "yes",
+			allow: "autoplay; fullscreen; encrypted-media; picture-in-picture",
+			frameborder: "0",						// style width color
+			name: "iframe",
 
-// Youtube source
-		related: 0,
+	// Youtube source
+			related: 0,
 
-// Vimeo source
-		author: 0,
-		portrait: 0,
-		title: 0,
+	// Vimeo source
+			author: 0,
+			portrait: 0,
+			title: 0,
 
-// Soundcloud source - make sure that muted is not true
-		color: "%23ff5500",
-		hide_related: true,
-		show_comments: false,
-		show_user: true,
-		show_reposts: false,
-		show_teaser: false,
-		visual: false,
+	// Soundcloud source
+			color: "%23ff5500",
+			hide_related: true,
+			show_comments: false,
+			show_user: true,
+			show_reposts: false,
+			show_teaser: false,
+			visual: false,
 
-// Bandcamp source - make sure that muted is not true
-		size: "large",								// "large" only if minimal is false
-		background_color: "ffffff",					// "RRGGBB" custom colors
-		link_color: "333333",						// "RRGGBB" custom colors
-		tracklist: true,
-		artwork: "small",							// small or big
-		transparent: true,
-		minimal: false,								// true only if size is false
+	// Bandcamp source
+			size: "large",								// "large" only if minimal is false
+			background_color: "ffffff",					// "RRGGBB" custom colors
+			link_color: "333333",						// "RRGGBB" custom colors
+			tracklist: true,
+			artwork: "small",							// small or big
+			transparent: true,
+			minimal: false,								// true only if size is false
 
-// Audiomack source - make sure that muted is not true
-		background: 1,								// 1 = art background, 0 = no background
-		color: false,								// "RRGGBB" custom colors or false
+	// Audiomack source
+			background: 1,								// 1 = art background, 0 = no background
+			color: false,								// "RRGGBB" custom colors or false
 	},
 
 	start: function() {
@@ -98,27 +98,27 @@ Module.register("yframe", {
 // iframe mode
 		} else if (this.config.mode == "iframe") {
 
-// Web iframe
+	// Web iframe
 			if (this.config.source == "Web") {
-				if (this.config.media == "media") {
-// media
+				if (this.config.media == "streaming") {
+		// streaming
 					iframe.src = this.config.url	+ "?autoplay=" + this.config.autoplay
 													+ "&mute=" + this.config.muted
 													+ "&controls=" + this.config.controls
 													+ "&loop=" + this.config.loop
-// website
-				} else if (this.config.media == "site") {
+		// website
+				} else if (this.config.media == "website") {
 					iframe.src = this.config.url
 				}
 
-// Youtube iframe
+	// Youtube iframe
 			} else if (this.config.source == "Youtube") {
 				iframe.src = this.config.url	+ "?autoplay=" + this.config.autoplay
 												+ "&mute=" + this.config.muted
 												+ "&controls=" + this.config.controls
 												+ "&loop=" + this.config.loop
 												+ "&rel=" + this.config.related;
-// Vimeo iframe
+	// Vimeo iframe
 			} else if (this.config.source == "Vimeo") {
 				iframe.src = this.config.url	+ "?byline=" + this.config.author
 												+ "&autoplay=" + this.config.autoplay
@@ -127,7 +127,7 @@ Module.register("yframe", {
 												+ "&portrait=" + this.config.portrait
 												+ "&title=" + this.config.title
 												+ "&loop=" + this.config.loop;
-// Soundcloud iframe
+	// Soundcloud iframe
 			} else if (this.config.source == "Soundcloud") {
 				iframe.src = this.config.url	+ "&color=" + this.config.color
 												+ "&auto_play=" + this.config.autoplay
@@ -137,7 +137,7 @@ Module.register("yframe", {
 												+ "&show_reposts=" + this.config.show_reposts
 												+ "&show_teaser=" + this.config.show_teaser
 												+ "&visual=" + this.config.visual;
-// Bandcamp iframe
+	// Bandcamp iframe
 			} else if (this.config.source == "Bandcamp") {
 				iframe.style.width = "700px";					// max 700px for Bandcamp
 				iframe.style.height = "470px";					// max 470px for Bandcamp
@@ -148,7 +148,7 @@ Module.register("yframe", {
 												+ "/artwork=" + this.config.artwork
 												+ "/minimal=" + this.config.minimal
 												+ "/transparent=" + this.config.transparent;
-// Audiomack iframe
+	// Audiomack iframe
 			} else if (this.config.source == "Audiomack") {
 				iframe.src = this.config.url	+ "?background=" + this.config.background
 												+ "&color=" + this.config.color;
